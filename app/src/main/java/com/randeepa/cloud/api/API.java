af539b6ebedfcda818354b30e3b2230a2bb8f8d9
Shamal Sandeep <ssdh.gdrive@gmail.com>
2020-10-29 17:35:51 +0530

    API change for new update

--- app/src/main/java/com/randeepa/cloud/api/API.java
+++ app/src/main/java/com/randeepa/cloud/api/API.java
@@ -1,7 +1,7 @@
 package com.randeepa.cloud.api;
 
 public class API {
-    private final String APILINK = "https://www.randeepa.cloud/android-api6";
+    private final String APILINK = "https://www.randeepa.cloud/android-api8";
 
     public String getApiLink() {
         return this.APILINK;
